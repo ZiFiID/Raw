dd3f136035c5902adf054807929965847417622b
ZiFiID <turenglobal@gmail.com>
2026-04-19 12:23:11 +0700

    MoonSec Capture [CONSTANTS_DUMP]

---
+++ Intercept_2026-04-19_05-23-10-608_4_CONSTANTS_DUMP.lua
@@ -0,0 +1,6 @@
+--[[ MOONSEC CONSTANT DUMP ]]
+-- This file contains the actual strings used by the script.
+
+[1] (string) = "This file was protected with MoonSec V3"
+[2] (string) = ".+"
+[4] (string) = "gsub"
