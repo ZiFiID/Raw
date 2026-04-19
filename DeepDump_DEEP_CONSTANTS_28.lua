cd5ae15bbfc42b944c58a93b19a1cba3cfc0aafe
ZiFiID <turenglobal@gmail.com>
2026-04-19 12:26:39 +0700

    Deep Intercept: DEEP_CONSTANTS

---
+++ DeepDump_DEEP_CONSTANTS_28.lua
@@ -0,0 +1,73 @@
+--[[ ZiFi Deep Scan Result ]]--
+[Depth 1][Const] print
+[Depth 1][Const] 
+Tested & working on:
+Ultra hatching simulator X
+Click game X
+Pet hatching simulator 99
+Clickers Adventure
+Clicker adventure
+Secret Tappers
+Mysterious Hatchers X
+Clicker legends
+Tapping Master
+Mega Hatchers X
+Ultimate tappers
+pet tappers
+Pet hatchers X
+tapping gods
+Godly tappers 99
+slushy tappers
+tapping mayhem
+egg tappers
+WWII Tycoon
+Undead-Rising-Tycoon
+Protect the house
+prison mining simulator
+pyro playground
+infections battleground
+The-1-000-000-Glass-Bridge
+Reborn as Driller
+[Depth 1][Const] getgenv
+[Depth 1][Const] Reborn As driller
+[Depth 1][Const] Protect the house
+[Depth 1][Const] Block Incremental
+[Depth 1][Const] Fun squid game! glass bridge 2
+[Depth 1][Const] The $1000000 glass bridge
+[Depth 1][Const] Prison mining simulator
+[Depth 1][Const] Golden Clickers
+[Depth 1][Const] Clickers Adventure
+[Depth 1][Const] Luck! Clickers Adventures!
+[Depth 1][Const] New Clicker Adventure
+[Depth 1][Const] Secret Tappers
+[Depth 1][Const] Mysterious Hatchers
+[Depth 1][Const] Clicker Legends
+[Depth 1][Const] Guess the killer
+[Depth 1][Const] Click Game X
+[Depth 1][Const] Tapping Masters
+[Depth 1][Const] Mega Hatchers X
+[Depth 1][Const] Pet hatching simulator reborn
+[Depth 1][Const] Pet Hatchers X
+[Depth 1][Const] Tapping Gods
+[Depth 1][Const] Godly Tappers 99
+[Depth 1][Const] Pet Hatching Simulator 99
+[Depth 1][Const] Ultra pet hatching simulator X
+[Depth 1][Const] SelectedGame
+[Depth 1][Const] Selected:
+None
+[Depth 1][Const] CreateLabel
+[Depth 1][Const] pairs
+[Depth 1][Const] holdColor
+[Depth 1][Const] Teleport to Game
+[Depth 1][Const] CreateButton
+[Depth 2][Const] ChangeText
+[Depth 2][Const] Selected: 
+[Depth 2][Const] TeleportService
+[Depth 2][Const] GetService
+[Depth 2][Const] Players
+[Depth 2][Const] LocalPlayer
+[Depth 2][Const] Teleport
+[Depth 2][Const] Library
+[Depth 2][Const] Error
+[Depth 2][Const] Please select a game first!
+[Depth 2][Const] CreateNotification
