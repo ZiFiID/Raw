4c5b202dacba36e910a9cb38eac00eac84005bf9
ZiFiID <turenglobal@gmail.com>
2026-04-19 12:26:29 +0700

    Deep Intercept: DEEP_CONSTANTS

---
+++ DeepDump_DEEP_CONSTANTS_12.lua
@@ -0,0 +1,8 @@
+--[[ ZiFi Deep Scan Result ]]--
+[Depth 1][Const] getgenv
+[Depth 1][Const] default
+[Depth 1][Const] Color
+[Depth 1][Const] TextColor
+[Depth 1][Const] loadstring
+[Depth 1][Const] https://raw.githubusercontent.com/rndmq/Serverlist/refs/heads/main/Server87
+[Depth 1][Const] HttpGet
