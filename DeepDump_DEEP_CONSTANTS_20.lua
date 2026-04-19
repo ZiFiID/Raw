d4b0988d9543a59188619e156a9d020803cd1ae5
ZiFiID <turenglobal@gmail.com>
2026-04-19 12:26:19 +0700

    Deep Intercept: DEEP_CONSTANTS

---
+++ DeepDump_DEEP_CONSTANTS_20.lua
@@ -0,0 +1,149 @@
+--[[ ZiFi Deep Scan Result ]]--
+[Depth 1][Const] getgenv
+[Depth 1][Const] MarketplaceService
+[Depth 1][Const] GetService
+[Depth 1][Const] PlaceId
+[Depth 1][Const] GetProductInfo
+[Depth 1][Const] https://www.roblox.com/games/
+[Depth 1][Const] Reporter
+[Depth 1][Const] CreateTab
+[Depth 1][Const] Player Info
+[Depth 1][Const] CreateSection
+[Depth 1][Const] HttpService
+[Depth 1][Const] Players
+[Depth 1][Const] LocalPlayer
+[Depth 1][Const] request
+[Depth 1][Const] fluxus
+[Depth 1][Const] Unknown Game
+[Depth 1][Const] string
+[Depth 1][Const] format
+[Depth 1][Const] Time Played: 0 seconds
+Username: %s
+Display Name: %s
+Player ID: %d
+Executor: %s
+Join Date: %s
+Membership: %s
+[Depth 1][Const] DisplayName
+[Depth 1][Const] UserId
+[Depth 1][Const] identifyexecutor
+[Depth 1][Const] Unknown
+[Depth 1][Const] %Y-%m-%d
+[Depth 1][Const] AccountAge
+[Depth 1][Const] MembershipType
+[Depth 1][Const] Premium
+[Depth 1][Const] CreateLabel
+[Depth 1][Const] spawn
+[Depth 1][Const] UpdateCD
+[Depth 1][Const] Requests, Reports & Reviews
+[Depth 1][Const] Cooldown
+[Depth 1][Const] Infor
+[Depth 1][Const] [ Beta ] New system.
+[Depth 1][Const] Report a Bug
+[Depth 1][Const] Describe the bug here...
+[Depth 1][Const] CreateTextBox
+[Depth 1][Const] Send Bug Report
+[Depth 1][Const] CreateButton
+[Depth 1][Const] Request a Feature
+[Depth 1][Const] Describe your feature request here...
+[Depth 1][Const] Send Feature Request
+[Depth 1][Const] SendEmbed
+[Depth 1][Const] Rating 
+[Depth 1][Const] CreateDropdown
+[Depth 1][Const] Write a Review
+[Depth 1][Const] Share your thoughts about the script...
+[Depth 1][Const] Send Review
+[Depth 2][Const] string
+[Depth 2][Const] format
+[Depth 2][Const] Time Played: %d seconds (%.2f minutes)
+Username: %s
+Display Name: %s
+Player ID: %d
+Executor: %s
+Join Date: %s
+Membership: %s
+[Depth 2][Const] DisplayName
+[Depth 2][Const] UserId
+[Depth 2][Const] identifyexecutor
+[Depth 2][Const] Unknown
+[Depth 2][Const] %Y-%m-%d
+[Depth 2][Const] AccountAge
+[Depth 2][Const] MembershipType
+[Depth 2][Const] Premium
+[Depth 2][Const] ChangeText
+[Depth 2][Const] spawn
+[Depth 2][Const] Library
+[Depth 2][Const] Cooldown Active
+[Depth 2][Const] Please wait 
+[Depth 2][Const]  seconds before sending another 
+[Depth 2][Const]  report!
+[Depth 2][Const] CreateNotification
+[Depth 2][Const] action
+[Depth 2][Const] title
+[Depth 2][Const] description
+[Depth 2][Const] playerName
+[Depth 2][Const] playerId
+[Depth 2][Const] gameName
+[Depth 2][Const] gameUrl
+[Depth 2][Const] submit
+[Depth 2][Const] UserId
+[Depth 2][Const] tostring
+[Depth 2][Const] pcall
+[Depth 2][Const] success
+[Depth 2][Const] Unknown
+[Depth 2][Const] uniqueId
+[Depth 2][Const] Report Sent Successfully!
+[Depth 2][Const] Your 
+[Depth 2][Const]  report has been submitted!
+Ticket ID: 
+[Depth 2][Const] 
+You'll receive notifications when admins respond!
+[Depth 2][Const] Awesome!
+[Depth 2][Const] Copy Ticket ID
+[Depth 2][Const] Delete me
+[Depth 2][Const] Failed to send report
+[Depth 2][Const] error
+[Depth 2][Const] Network error: 
+[Depth 2][Const] Error
+[Depth 2][Const] Bug in 
+[Depth 2][Const] Describe the bug here...
+[Depth 2][Const] Library
+[Depth 2][Const] Error
+[Depth 2][Const] Please provide a bug description!
+[Depth 2][Const] CreateNotification
+[Depth 2][Const] string
+[Depth 2][Const] Bug description must be at least 10 characters long!
+[Depth 2][Const] Feature Request for 
+[Depth 2][Const] Describe your feature request here...
+[Depth 2][Const] Library
+[Depth 2][Const] Error
+[Depth 2][Const] Please provide a feature description!
+[Depth 2][Const] CreateNotification
+[Depth 2][Const] string
+[Depth 2][Const] Feature description must be at least 10 characters long!
+[Depth 2][Const] feature
+[Depth 2][Const] HttpService
+[Depth 2][Const] GetService
+[Depth 2][Const] application/json
+[Depth 2][Const] Content-Type
+[Depth 2][Const] embeds
+[Depth 2][Const] JSONEncode
+[Depth 2][Const] pcall
+[Depth 2][Const] Cooldown Active
+[Depth 2][Const] Please wait before sending another review!
+[Depth 2][Const] CreateNotification
+[Depth 2][Const] UpdateCD
+[Depth 2][Const] string
+[Depth 2][Const] format
+[Depth 2][Const] **Review:** %s
+**Player:** %s
+**Game:** [%s](%s)
+**Rating:** %s
+[Depth 2][Const] No review provided
+[Depth 2][Const] New Script Review
+[Depth 2][Const] title
+[Depth 2][Const] description
+[Depth 2][Const] color
+[Depth 2][Const] SendEmbed
+[Depth 2][Const] https://discord.com/api/webhooks/1349733279422615572/qnJED-CprHbOT3TWn2_Jl6mPblfpuiryCqS58liJK2Y61i7ghhpyyV9X7bQm_q5BLBqs
+[Depth 2][Const] Your review has been sent!
